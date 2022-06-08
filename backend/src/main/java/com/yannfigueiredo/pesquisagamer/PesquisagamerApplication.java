7ec0c2699b5307cc82e0701b6cd5d113db3b2e66
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-06-08 12:11:34 -0300

    create project

---
+++ backend/src/main/java/com/yannfigueiredo/pesquisagamer/PesquisagamerApplication.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.pesquisagamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisagamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisagamerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yannfigueiredo/pesquisagamer/PesquisagamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.pesquisagamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisagamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
