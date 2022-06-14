f7691321053e96ca7aebc8b057eea3a7bb67d38d
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-06-14 17:14:59 -0300

    ready for production

--- backend/src/main/java/com/yannfigueiredo/gamerpreferencesurvey/repositories/VoteRepository.java
+++ backend/src/main/java/com/yannfigueiredo/gamerpreferencesurvey/repositories/VoteRepository.java
@@ -13,12 +13,9 @@ import com.yannfigueiredo.gamerpreferencesurvey.entities.Vote;
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 	
-	/*Query para PostgreSQL@Query("SELECT obj FROM Vote obj WHERE "
-			+ "(coalesce(:minDate, null) IS NULL OR obj.date >= :minDate) AND "
-			+ "(coalesce(:maxDate, null) IS NULL OR obj.date <= :maxDate)")*/
 	@Query("SELECT obj FROM Vote obj WHERE "
-			+ "(:minDate IS NULL OR obj.date >= :minDate) AND "
-			+ "(:maxDate IS NULL OR obj.date <= :maxDate)")
+			+ "(coalesce(:minDate, null) IS NULL OR obj.date >= :minDate) AND "
+			+ "(coalesce(:maxDate, null) IS NULL OR obj.date <= :maxDate)")
 	Page<Vote> findByDates(Instant minDate, Instant maxDate, Pageable pageable);
 
 }
